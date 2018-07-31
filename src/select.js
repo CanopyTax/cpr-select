567baef01bd747d718b9c8e2c4c3b55107345319
Jonathan Junca <jonathanjunca@Jonathans-MacBook-Pro.local>
2018-07-31 14:48:18 -0600

    added a topOffset prop that can be used instead of the value calculated by the positionDialogAndGetTop function

--- src/select.js
+++ src/select.js
@@ -5,271 +5,271 @@ let searchString = "";
 let keyTimeout;
 
 function nearest(element, el) {
-	if (!element) return false;
-	return element === el || nearest(element.parentElement, el);
+  if (!element) return false;
+  return element === el || nearest(element.parentElement, el);
 }
 
 export default class CanopySelect extends React.Component {
 
-	state = {
-		dialogDisplayed: false,
-		top: 0,
-		focused: false,
-		close: (e) => {
-			if (!nearest(e.target, this.el)) {
-				this.setState({
-					dialogDisplayed: false,
-					focused: false
-				});
-			}
-		}
-	};
+  state = {
+    dialogDisplayed: false,
+    top: 0,
+    focused: false,
+    close: (e) => {
+      if (!nearest(e.target, this.el)) {
+        this.setState({
+          dialogDisplayed: false,
+          focused: false
+        });
+      }
+    }
+  };
 
-	componentWillUnmount() {
-		document.body.removeEventListener('click', this.state.close);
-	}
+  componentWillUnmount() {
+    document.body.removeEventListener('click', this.state.close);
+  }
 
-	componentDidMount() {
-		document.body.addEventListener('click', this.state.close);
-		let index = this.getIndex(this.props.selected);
-		this.setState(() => ({
-			selectedIndex: index === -1 ? 0 : index,
-		}));
-	}
+  componentDidMount() {
+    document.body.addEventListener('click', this.state.close);
+    let index = this.getIndex(this.props.selected);
+    this.setState(() => ({
+      selectedIndex: index === -1 ? 0 : index,
+    }));
+  }
 
-	displayDialog = (e) => {
-		if (this.disabled()) return;
+  displayDialog = (e) => {
+    if (this.disabled()) return;
 
-		this.setState({
-			dialogDisplayed: true
-		})
-	};
+    this.setState({
+      dialogDisplayed: true
+    })
+  };
 
-	getIndex = (key) => {
-		return findIndex(this.props.options, {key: key});
-	};
+  getIndex = (key) => {
+    return findIndex(this.props.options, {key: key});
+  };
 
-	onKeyDown = (e) => {
-		if (this.disabled()) return;
+  onKeyDown = (e) => {
+    if (this.disabled()) return;
 
-		const key = e.which;
-		let selectedIndex = this.state.selectedIndex;
+    const key = e.which;
+    let selectedIndex = this.state.selectedIndex;
 
-		if(key !== 9) {			// tab key
-			e.preventDefault();
-		}
+    if(key !== 9) {			// tab key
+      e.preventDefault();
+    }
 
-		if(key === 13) {				// enter key
-			this.selectItem(selectedIndex)
-		} else if(key === 38) { // up key
-			if (selectedIndex <= 0) {
-				this.setState({
-					dialogDisplayed: true,
-				});
-			} else {
-				this.setState({
-					dialogDisplayed: true,
-					selectedIndex: selectedIndex - 1
-				});
-			}
+    if(key === 13) {				// enter key
+      this.selectItem(selectedIndex)
+    } else if(key === 38) { // up key
+      if (selectedIndex <= 0) {
+        this.setState({
+          dialogDisplayed: true,
+        });
+      } else {
+        this.setState({
+          dialogDisplayed: true,
+          selectedIndex: selectedIndex - 1
+        });
+      }
 
-		} else if (key === 40) { // down key
+    } else if (key === 40) { // down key
 
-			if (selectedIndex === this.props.options.length - 1) {
-				this.setState({
-					dialogDisplayed: true
-				});
-			} else {
-				this.setState({
-					dialogDisplayed: true,
-					selectedIndex: selectedIndex + 1
-				});
-			}
-		} else if(key === 27) { // escape key
-			this.setState({
-				dialogDisplayed: false
-			});
-		} else {								// all other keys
-			this.highlightByText(e.which);
-		}
-	};
+      if (selectedIndex === this.props.options.length - 1) {
+        this.setState({
+          dialogDisplayed: true
+        });
+      } else {
+        this.setState({
+          dialogDisplayed: true,
+          selectedIndex: selectedIndex + 1
+        });
+      }
+    } else if(key === 27) { // escape key
+      this.setState({
+        dialogDisplayed: false
+      });
+    } else {								// all other keys
+      this.highlightByText(e.which);
+    }
+  };
 
-	triggerItemChange = () => {
-		if (this.props.onChange) {
-			this.props.onChange.call(
-				null,
-				this.props.options[this.state.selectedIndex].key,
-				this.props.options[this.state.selectedIndex],
-				this.state.selectedIndex
-			);
-		}
-	};
+  triggerItemChange = () => {
+    if (this.props.onChange) {
+      this.props.onChange.call(
+        null,
+        this.props.options[this.state.selectedIndex].key,
+        this.props.options[this.state.selectedIndex],
+        this.state.selectedIndex
+      );
+    }
+  };
 
-	selectItem = (index, e) => {
-		if (this.disabled()) return;
+  selectItem = (index, e) => {
+    if (this.disabled()) return;
 
-		setTimeout(() => {
-			this.setState({
-				selectedIndex: index,
-				focused: true,
-				dialogDisplayed: false
-			});
-			setTimeout(this.triggerItemChange);
-		});
-	};
+    setTimeout(() => {
+      this.setState({
+        selectedIndex: index,
+        focused: true,
+        dialogDisplayed: false
+      });
+      setTimeout(this.triggerItemChange);
+    });
+  };
 
-	positionDialogAndGetTop = (options, index, maxHeight) => {
-		const distanceFromEnd = options.length - index;
-		const numVisibleOptions = Math.floor(maxHeight / 36);
-		const numSurroundingOptions = Math.floor((numVisibleOptions - 1) / 2);
-		const unusedPixels = maxHeight % 36;
+  positionDialogAndGetTop = (options, index, maxHeight) => {
+    const distanceFromEnd = options.length - index;
+    const numVisibleOptions = Math.floor(maxHeight / 36);
+    const numSurroundingOptions = Math.floor((numVisibleOptions - 1) / 2);
+    const unusedPixels = maxHeight % 36;
 
-		if (index > numSurroundingOptions && distanceFromEnd < numSurroundingOptions + 1) {
-			// Bottom 5
-			if (options.length < numVisibleOptions) {
-				// Dialog doesn't have a scroll
-				return -2 + (36 * index * -1 - 10) + "px";
-			} else {
-				// Dialog has a scroll
-				this.positionDialog(index, maxHeight);
-				const start = (maxHeight / -2) - 15;
-				return start - (numSurroundingOptions - distanceFromEnd) * 36 + "px";
-			}
-		} else if (index > numSurroundingOptions) {
-			// Middle
-			this.positionDialog(index, maxHeight);
-			return (maxHeight / -2) - 0.0075 * maxHeight;
-		} else {
-			// Top 5
-			return -1 + (36 * index * -1 - 10) + "px";
-		}
-	};
+    if (index > numSurroundingOptions && distanceFromEnd < numSurroundingOptions + 1) {
+      // Bottom 5
+      if (options.length < numVisibleOptions) {
+        // Dialog doesn't have a scroll
+        return -2 + (36 * index * -1 - 10) + "px";
+      } else {
+        // Dialog has a scroll
+        this.positionDialog(index, maxHeight);
+        const start = (maxHeight / -2) - 15;
+        return start - (numSurroundingOptions - distanceFromEnd) * 36 + "px";
+      }
+    } else if (index > numSurroundingOptions) {
+      // Middle
+      this.positionDialog(index, maxHeight);
+      return (maxHeight / -2) - 0.0075 * maxHeight;
+    } else {
+      // Top 5
+      return -1 + (36 * index * -1 - 10) + "px";
+    }
+  };
 
-	positionDialog = (index, maxHeight) => {
-		setTimeout(() => {
-			let menuDialog = this.el.querySelector(".cp-select__menu");
-			if (menuDialog) {
-				const dialogHeightImpact = maxHeight / 2 - 8;
-				menuDialog.scrollTop = (36 * index - dialogHeightImpact);
-			}
-		});
-	};
-
-	focusSelect = () => {
-		if (!this.state.focused) {
-			this.setState({
-				focused: true
-			});
-		}
-	};
-
-	onBlur = () => {
-		this.setState({
-			focused: false
-    }, () => {
-      if (this.props.onBlur) {
-			this.props.onBlur.call(
-				null,
-				this.props.options[this.state.selectedIndex].key,
-				this.props.options[this.state.selectedIndex],
-				this.state.selectedIndex
-			);
+  positionDialog = (index, maxHeight) => {
+    setTimeout(() => {
+      let menuDialog = this.el.querySelector(".cp-select__menu");
+      if (menuDialog) {
+        const dialogHeightImpact = maxHeight / 2 - 8;
+        menuDialog.scrollTop = (36 * index - dialogHeightImpact);
       }
     });
-	};
+  };
 
-	highlightByText = (charCode) => {
-		searchString += String.fromCharCode(charCode);
-		let i = this.getIndexFromString(searchString);
+  focusSelect = () => {
+    if (!this.state.focused) {
+      this.setState({
+        focused: true
+      });
+    }
+  };
 
-		if(i > -1) {
-			this.selectItem(i);
-		}
+  onBlur = () => {
+    this.setState({
+      focused: false
+    }, () => {
+      if (this.props.onBlur) {
+      this.props.onBlur.call(
+        null,
+        this.props.options[this.state.selectedIndex].key,
+        this.props.options[this.state.selectedIndex],
+        this.state.selectedIndex
+      );
+      }
+    });
+  };
 
-		clearTimeout(keyTimeout);
+  highlightByText = (charCode) => {
+    searchString += String.fromCharCode(charCode);
+    let i = this.getIndexFromString(searchString);
 
-		keyTimeout = setTimeout(function() {
-			searchString = "";
-		}, 1000);
-	};
+    if(i > -1) {
+      this.selectItem(i);
+    }
 
-	getIndexFromString = (searchString) => {
-		searchString = searchString.toLowerCase();
-		return findIndex(this.props.options, (option) => {
-			return this.getViewValue(option) !== null ? this.getViewValue(option).toLowerCase().indexOf(searchString) === 0 : false;
-		});
-	};
+    clearTimeout(keyTimeout);
 
-	getViewValue = (option) => {
-		if (option.value === null || option.value === undefined) return null;
-		return option.value || option;
-	};
+    keyTimeout = setTimeout(function() {
+      searchString = "";
+    }, 1000);
+  };
 
-	getDialog = (dialogDisplayed, options) => {
-		if (dialogDisplayed) {
+  getIndexFromString = (searchString) => {
+    searchString = searchString.toLowerCase();
+    return findIndex(this.props.options, (option) => {
+      return this.getViewValue(option) !== null ? this.getViewValue(option).toLowerCase().indexOf(searchString) === 0 : false;
+    });
+  };
 
-			let selectedIndex = this.state.selectedIndex;
+  getViewValue = (option) => {
+    if (option.value === null || option.value === undefined) return null;
+    return option.value || option;
+  };
 
-			let optionElements = options.map((option, index) => {
-				if (option.separator) {
-					return <li key={`separator${index}`} className="separator" />
-				} else {
-					return <li key={option.key} className={selectedIndex === index ? '+selected' : ''} onMouseDown={this.selectItem.bind(this, index)}><a style={option.value !== null ? {} : {color: "rgba(0,0,0,0)"}}>{option.value !== null ? option.value : "null"}</a></li>
-				}
-			});
+  getDialog = (dialogDisplayed, options, topOffset) => {
+    if (dialogDisplayed) {
 
-			setTimeout(() => {
-				try {
-					this.el.querySelector('.cp-select__hidden-input').focus();
-				} catch(e) {
-					// It is okay if the element does not exist anymore
-					if (e.message.indexOf('Invariant Violation') === -1) {
-						throw new Error(e.message);
-					}
-				}
-			}, 100);
+      let selectedIndex = this.state.selectedIndex;
+
+      let optionElements = options.map((option, index) => {
+        if (option.separator) {
+          return <li key={`separator${index}`} className="separator" />
+        } else {
+          return <li key={option.key} className={selectedIndex === index ? '+selected' : ''} onMouseDown={this.selectItem.bind(this, index)}><a style={option.value !== null ? {} : {color: "rgba(0,0,0,0)"}}>{option.value !== null ? option.value : "null"}</a></li>
+        }
+      });
+
+      setTimeout(() => {
+        try {
+          this.el.querySelector('.cp-select__hidden-input').focus();
+        } catch(e) {
+          // It is okay if the element does not exist anymore
+          if (e.message.indexOf('Invariant Violation') === -1) {
+            throw new Error(e.message);
+          }
+        }
+      }, 100);
 
       const maxHeight = this.props.maxHeight || 400;
       const zIndex = this.props.zIndex || 1000
-			return (
-				<div>
-					<ul className="cp-select__menu cps-dropdown-menu" style={{top: this.positionDialogAndGetTop(options, selectedIndex, maxHeight), maxHeight: maxHeight + 'px', zIndex}}>
-						{optionElements}
-					</ul>
-				</div>
-			);
-		}
-	};
+      return (
+        <div>
+          <ul className="cp-select__menu cps-dropdown-menu" style={{top: topOffset ? topOffset + 'px' : this.positionDialogAndGetTop(options, selectedIndex, maxHeight), maxHeight: maxHeight + 'px', zIndex}}>
+            {optionElements}
+          </ul>
+        </div>
+      );
+    }
+  };
 
-	disabled = () => {
-		return !this.props.options || this.props.options.length === 0 || this.props.disabled;
-	}
+  disabled = () => {
+    return !this.props.options || this.props.options.length === 0 || this.props.disabled;
+  }
 
-	render = () => {
-		const {options = [], selected, outerClass, selectClass, placeholder } = this.props;
-		let cpSelectClasses = 'cp-select';
-		let selectedItem = options[
-			this.getIndex(selected)
-		];
-		let that = this;
+  render = () => {
+    const {options = [], selected, outerClass, selectClass, placeholder, topOffset } = this.props;
+    let cpSelectClasses = 'cp-select';
+    let selectedItem = options[
+      this.getIndex(selected)
+    ];
+    let that = this;
 
-		if (this.disabled()) cpSelectClasses += ' +disabled';
-		if (this.state.focused) cpSelectClasses += ' +focus';
+    if (this.disabled()) cpSelectClasses += ' +disabled';
+    if (this.state.focused) cpSelectClasses += ' +focus';
 
-		return (
-			<div ref={function(el) { if (el) that.el = el; }} className={`cp-select-outer ${outerClass ? outerClass : ''}`} role='select'>
-				<input className="cp-select__hidden-input" onFocus={this.focusSelect} onBlur={this.onBlur} onKeyDown={this.onKeyDown}/>
-				<div className={`${cpSelectClasses} ${selectClass ? selectClass : ''}`} onClick={this.displayDialog}>
-					{selectedItem
-						? <div className="cp-select__selected">{selectedItem.value}</div>
-						: <div className="cp-select__selected" style={{color:'#afafaf'}}>{placeholder}</div>
-					}
-					{options.length > 0 && <div className="cp-select__icon"></div>}
-				</div>
-				{this.getDialog(this.state.dialogDisplayed, options)}
-			</div>
-		);
-	}
+    return (
+      <div ref={function(el) { if (el) that.el = el; }} className={`cp-select-outer ${outerClass ? outerClass : ''}`} role='select'>
+        <input className="cp-select__hidden-input" onFocus={this.focusSelect} onBlur={this.onBlur} onKeyDown={this.onKeyDown}/>
+        <div className={`${cpSelectClasses} ${selectClass ? selectClass : ''}`} onClick={this.displayDialog}>
+          {selectedItem
+            ? <div className="cp-select__selected">{selectedItem.value}</div>
+            : <div className="cp-select__selected" style={{color:'#afafaf'}}>{placeholder}</div>
+          }
+          {options.length > 0 && <div className="cp-select__icon"></div>}
+        </div>
+        {this.getDialog(this.state.dialogDisplayed, options, topOffset)}
+      </div>
+    );
+  }
 }
 
 if (typeof window !== "undefined" && window && !window.CanopySelect) window.CanopySelect = CanopySelect;
--- src/select.spec.js
+++ src/select.spec.js
@@ -4,142 +4,142 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
 
 describe('select', function() {
-	it('Should render an empty component', function() {
-		let multiSelect = TestUtils.renderIntoDocument(
-			<CanopySelect options={[]}></CanopySelect>
-		);
+  it('Should render an empty component', function() {
+    let multiSelect = TestUtils.renderIntoDocument(
+      <CanopySelect options={[]}></CanopySelect>
+    );
 
-		let hiddenInput = TestUtils.scryRenderedDOMComponentsWithTag(multiSelect, 'input');
-		let renderedInput = TestUtils.scryRenderedDOMComponentsWithClass(multiSelect, 'cp-select');
+    let hiddenInput = TestUtils.scryRenderedDOMComponentsWithTag(multiSelect, 'input');
+    let renderedInput = TestUtils.scryRenderedDOMComponentsWithClass(multiSelect, 'cp-select');
 
-		expect(hiddenInput.length).toBe(1);
-		expect(renderedInput.length).toBe(1);
-	});
+    expect(hiddenInput.length).toBe(1);
+    expect(renderedInput.length).toBe(1);
+  });
 
-	it('Should render placeholder', function() {
-		let multiSelect = TestUtils.renderIntoDocument(
-			<CanopySelect options={[]} placeholder='Kifak?'></CanopySelect>
-		);
+  it('Should render placeholder', function() {
+    let multiSelect = TestUtils.renderIntoDocument(
+      <CanopySelect options={[]} placeholder='Kifak?'></CanopySelect>
+    );
 
-		let selection = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select__selected');
-		expect(ReactDOM.findDOMNode(selection).textContent).toEqual('Kifak?');
-	});
+    let selection = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select__selected');
+    expect(ReactDOM.findDOMNode(selection).textContent).toEqual('Kifak?');
+  });
 
-	it('Should render a component with a selected option', function() {
-		let items = [
-			{
-				"value": "Alabama",
-				"key": "AL"
-			}, {
-				"value": "Alaska",
-				"key": "AK"
-			}, {
-				"value": "American Samoa",
-				"key": "AS"
-			}
-		];
+  it('Should render a component with a selected option', function() {
+    let items = [
+      {
+        "value": "Alabama",
+        "key": "AL"
+      }, {
+        "value": "Alaska",
+        "key": "AK"
+      }, {
+        "value": "American Samoa",
+        "key": "AS"
+      }
+    ];
 
-		let multiSelect = TestUtils.renderIntoDocument(
-			<CanopySelect options={items} selected={items[1].key}></CanopySelect>
-		);
+    let multiSelect = TestUtils.renderIntoDocument(
+      <CanopySelect options={items} selected={items[1].key}></CanopySelect>
+    );
 
-		let selection = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select__selected');
-		expect(ReactDOM.findDOMNode(selection).textContent).toEqual('Alaska');
-	});
+    let selection = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select__selected');
+    expect(ReactDOM.findDOMNode(selection).textContent).toEqual('Alaska');
+  });
 
-	it('Should render a dialog when clicked', function() {
-		let items = [
-			{
-				"value": "Alabama",
-				"key": "AL"
-			}, {
-				"value": "Alaska",
-				"key": "AK"
-			}, {
-				"value": "American Samoa",
-				"key": "AS"
-			}
-		];
+  it('Should render a dialog when clicked', function() {
+    let items = [
+      {
+        "value": "Alabama",
+        "key": "AL"
+      }, {
+        "value": "Alaska",
+        "key": "AK"
+      }, {
+        "value": "American Samoa",
+        "key": "AS"
+      }
+    ];
 
-		let multiSelect = TestUtils.renderIntoDocument(
-			<CanopySelect options={items}></CanopySelect>
-		);
+    let multiSelect = TestUtils.renderIntoDocument(
+      <CanopySelect options={items}></CanopySelect>
+    );
 
-		let select = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select');
-		TestUtils.Simulate.click(select);
+    let select = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select');
+    TestUtils.Simulate.click(select);
 
-		let menu = TestUtils.scryRenderedDOMComponentsWithClass(multiSelect, 'cp-select__menu');
-		expect(menu.length).toBe(1);
-		expect(ReactDOM.findDOMNode(menu[0]).querySelectorAll('li').length).toBe(3);
-	});
+    let menu = TestUtils.scryRenderedDOMComponentsWithClass(multiSelect, 'cp-select__menu');
+    expect(menu.length).toBe(1);
+    expect(ReactDOM.findDOMNode(menu[0]).querySelectorAll('li').length).toBe(3);
+  });
 
-	it('Should select items and trigger a callback', function(run) {
-		let items = [
-			{
-				"value": "Alabama",
-				"key": "AL"
-			}, {
-				"value": "Alaska",
-				"key": "AK"
-			}, {
-				"value": "American Samoa",
-				"key": "AS"
-			}
-		];
+  it('Should select items and trigger a callback', function(run) {
+    let items = [
+      {
+        "value": "Alabama",
+        "key": "AL"
+      }, {
+        "value": "Alaska",
+        "key": "AK"
+      }, {
+        "value": "American Samoa",
+        "key": "AS"
+      }
+    ];
 
-		function callback(key, item, index) {
-			expect(key).toBe('AS');
-			expect(item.key).toBe('AS');
-			expect(index).toBe(2);
-			run();
-		}
+    function callback(key, item, index) {
+      expect(key).toBe('AS');
+      expect(item.key).toBe('AS');
+      expect(index).toBe(2);
+      run();
+    }
 
-		let multiSelect = TestUtils.renderIntoDocument(
-			<CanopySelect options={items} onChange={callback}></CanopySelect>
-		);
+    let multiSelect = TestUtils.renderIntoDocument(
+      <CanopySelect options={items} onChange={callback}></CanopySelect>
+    );
 
-		let select = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select');
-		TestUtils.Simulate.click(select);
+    let select = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select');
+    TestUtils.Simulate.click(select);
 
-		TestUtils.Simulate.mouseDown(
-			ReactDOM.findDOMNode(
-				ReactDOM.findDOMNode(multiSelect).querySelectorAll('li')[2]
-			)
-		);
-	});
+    TestUtils.Simulate.mouseDown(
+      ReactDOM.findDOMNode(
+        ReactDOM.findDOMNode(multiSelect).querySelectorAll('li')[2]
+      )
+    );
+  });
 
-	it('Should not allow up arrow to go out of bounds', function(run) {
-		let items = [
-			{
-				"value": "Alabama",
-				"key": "AL"
-			}, {
-				"value": "Alaska",
-				"key": "AK"
-			}, {
-				"value": "American Samoa",
-				"key": "AS"
-			}
-		];
+  it('Should not allow up arrow to go out of bounds', function(run) {
+    let items = [
+      {
+        "value": "Alabama",
+        "key": "AL"
+      }, {
+        "value": "Alaska",
+        "key": "AK"
+      }, {
+        "value": "American Samoa",
+        "key": "AS"
+      }
+    ];
 
-		function callback(key, item, index) {
-			expect(key).toBe('AL');
-			expect(item.key).toBe('AL');
-			expect(index).toBe(0);
-			run();
-		}
+    function callback(key, item, index) {
+      expect(key).toBe('AL');
+      expect(item.key).toBe('AL');
+      expect(index).toBe(0);
+      run();
+    }
 
-		let multiSelect = TestUtils.renderIntoDocument(
-			<CanopySelect options={items} onChange={callback}></CanopySelect>
-		);
+    let multiSelect = TestUtils.renderIntoDocument(
+      <CanopySelect options={items} onChange={callback}></CanopySelect>
+    );
 
-		let select = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select');
-		TestUtils.Simulate.click(select);
-		let input = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select__hidden-input');
-		TestUtils.Simulate.keyDown(input, {which: 38});
-		TestUtils.Simulate.keyDown(input, {which: 38});
-		TestUtils.Simulate.keyDown(input, {which: 38});
-		TestUtils.Simulate.keyDown(input, {which: 38});
-		TestUtils.Simulate.keyDown(input, {which: 13});
-	});
+    let select = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select');
+    TestUtils.Simulate.click(select);
+    let input = TestUtils.findRenderedDOMComponentWithClass(multiSelect, 'cp-select__hidden-input');
+    TestUtils.Simulate.keyDown(input, {which: 38});
+    TestUtils.Simulate.keyDown(input, {which: 38});
+    TestUtils.Simulate.keyDown(input, {which: 38});
+    TestUtils.Simulate.keyDown(input, {which: 38});
+    TestUtils.Simulate.keyDown(input, {which: 13});
+  });
 });
